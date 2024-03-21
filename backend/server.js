d0b310a23a7e4b4737542b6466d9df4af16daa37
Nikhil-Biyani <nikhilbiyani2406@gmail.com>
2024-03-21 17:05:24 +0530

    completed backend routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
-import authRoutes from "./routes/authRoutes.js"
+import authRoutes from "./routes/authRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -10,7 +14,11 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json()); // to parse incoming requests with JSON payloads (from req.body)
+app.use(cookieParser()); // to parse incoming cookies from req.cookies
+
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 // app.get("/", (req, res) => {
 //     // root route
